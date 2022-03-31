92a906d4eafe9fd5962572250337cae480a9e6f4
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-03-31 21:20:29 +0900

    add-mongodb-connect-function

--- ticket/auth/src/index.ts
+++ ticket/auth/src/index.ts
@@ -1,11 +1,13 @@
 import { errorHandler } from "./middleware/error-handlers";
 import express from "express";
+import mongoose from "mongoose";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { NotFoundError } from "./errors/not-found-error";
+import "express-async-errors";
 
 const app = express();
 
@@ -15,11 +17,24 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-app.get("*", () => {
+
+// we can use async not using next because of express-async-errors
+app.all("*", async () => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb:auth-mongo-srv:27017/");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
