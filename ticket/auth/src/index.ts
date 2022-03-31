28b322c7ac7c131f9497e9371d2039a42c8d5355
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-03-31 20:50:10 +0900

    add not found error

--- ticket/auth/src/index.ts
+++ ticket/auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.get("*", () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
--- ticket/auth/src/middleware/error-handlers.ts
+++ ticket/auth/src/middleware/error-handlers.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DataBaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,17 +7,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({
       errors: err.serializeErrors(),
     });
   }
 
-  if (err instanceof DataBaseConnectionError) {
-    return res.status(err.statusCode).send({
-      errors: err.serializeErrors(),
-    });
-  }
   res.status(400).send({
     error: [{ message: "Something went wrong" }],
   });
