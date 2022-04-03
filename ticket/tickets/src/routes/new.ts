5b0c480202d847a18a4a252a1756b4b72ab99cfa
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-03 11:23:55 +0900

    change ticket install module

--- ticket/tickets/src/routes/new.ts
+++ ticket/tickets/src/routes/new.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
-import { requireAuth } from '@cygnetops/common';
+import express, { Request, Response } from "express";
+import { requireAuth } from "@taitasudev5/common";
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
