a435851e92b2427910b64f6d5821109dec4a682c
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-03 09:07:29 +0900

    add-ticket-microservices

---
+++ ticket/tickets/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import { errorHandler, NotFoundError } from "@taitasudev5/common";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ ticket/tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+import { request } from "supertest";
+import { app } from "../../app";
+
+it("has a route handler listening to api/tickets for post requires", async () => {});
+it("can only be accessed if the user sign in", async () => {});
+it("returns an error if an invalid title is provided", async () => {});
+it("return an error if a invalid prices provided", async () => {});
+it("create an valid ticket", async () => {});
