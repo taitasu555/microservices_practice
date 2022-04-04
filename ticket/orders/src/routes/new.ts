ec0c2bd64a54aa8a5ca37bfb089c5cada6e03ea6
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-04 23:51:16 +0900

    Adding-create-order

--- ticket/orders/src/routes/new.ts
+++ ticket/orders/src/routes/new.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
-import { requireAuth, validationRequest } from "@taitasudev5/common";
+import {
+  requireAuth,
+  validationRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from "@taitasudev5/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
+
+// const isReserved = await ticket.isReserved();
 
 router.post(
   "/api/orders",
@@ -17,7 +27,26 @@ router.post(
   ],
   validationRequest,
   async (req: Request, res: Response) => {
-    res.send("Orders");
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    //  Make sure the ticket not already reserved
+    // run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the order status is *not* cancelled
+    // if we find an order from that means the ticket *is* reserved
+
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+    // Calculate an expiration date for the order
+
+    // Build the order and save it to the database
+
+    // publish an event saying that an order was created
   }
 );
 
