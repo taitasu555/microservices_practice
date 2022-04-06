4c59e3345a904e2e54b6c36c5690011107b69647
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-06 18:36:22 +0900

    create container file

--- expiration/src/app.ts
+++ expiration/src/app.ts
@@ -3,10 +3,6 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@taitasudev5/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -18,11 +14,6 @@ app.use(
   })
 );
 app.use(currentUser);
-
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
