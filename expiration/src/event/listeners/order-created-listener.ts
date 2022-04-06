a93df653fd962119ecd8d2d23537780dc401d1fe
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-06 18:47:31 +0900

    create-listner-event-in-exipration

--- expiration/src/event/listeners/order-created-listener.ts
+++ expiration/src/event/listeners/order-created-listener.ts
@@ -1,1 +1,8 @@
-import { Listener } from "@taitasudev5/common";
+import { queueGroupName } from "./queue-group-name";
+import { Listener, OrderCreatedEvent, Subjects } from "@taitasudev5/common";
+import { Message } from "node-nats-streaming";
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
