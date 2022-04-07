daf9eb19054087be1946b66851a2be60a18928f1
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-07 14:34:37 +0900

    add-paymnets-test

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,27 @@
+import { queueGroupName } from "./queue-group-name";
+import {
+  OrderCreatedEvent,
+  OrderStatus,
+  Listener,
+  Subjects,
+} from "@taitasudev5/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
