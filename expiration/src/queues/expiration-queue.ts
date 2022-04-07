2b9b99f9ba560996e8466f7a0b85e0692ab7beab
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-07 13:06:43 +0900

    fix delay time in expiration

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,12 +1,12 @@
-import Queue from 'bull';
-import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
-import { natsWrapper } from '../nats-wrapper';
+import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 interface Payload {
   orderId: string;
 }
 
-const expirationQueue = new Queue<Payload>('order:expiration', {
+const expirationQueue = new Queue<Payload>("order:expiration", {
   redis: {
     host: process.env.REDIS_HOST,
   },
