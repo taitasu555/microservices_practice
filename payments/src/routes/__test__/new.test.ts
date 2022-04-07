fe6daa142540ea42ee102a62e0b6be1dadb241b5
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-07 16:58:24 +0900

    test of payments

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 import mongoose from "mongoose";
+import { OrderStatus } from "@taitasudev5/common";
 
 it("returns order not exists", async () => {
   await request(app)
@@ -14,6 +15,43 @@ it("returns order not exists", async () => {
     .expect(404);
 });
 
-it("return a 401 when order does not belong to user ", async () => {});
+it("return a 401 when order does not belong to user ", async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
 
-it("return a 400 when order was cancelled", async () => {});
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "asdasdasd",
+      orderId: order.id,
+    })
+    .expect(401);
+});
+
+it("return a 400 when order was cancelled", async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(userId))
+    .send({
+      token: "asdasdasd",
+      orderId: order.id,
+    })
+    .expect(400);
+});
