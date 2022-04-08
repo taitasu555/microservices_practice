fb5c1d378c56fc73adc935e62eaa6407d0e71b3f
ts-taishin.sato <ts-taishin.sato@rakuten.com>
2022-04-08 12:06:18 +0900

    change ticket create ui

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,32 @@
 import buildClient from "../api/build-client";
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket) => (
+            <tr key={ticket.id}>
+              <td>{ticket.title}</td>
+              <td>{ticket.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-// export async function getServerSideProps(context) {
-//   return {};
-// }
-
 LandingPage.getInitialProps = async (appContext, client, currentUser) => {
-  console.log(currentUser);
-  return {};
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
